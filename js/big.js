4b9457182806472669bd7d694b0321fa23f139ce
TSAICHIAYEN <34646740+TSAICHIAYEN@users.noreply.github.com>
2018-06-10 17:23:40 +0800

    fin

---
+++ js/big.js
@@ -0,0 +1,9 @@
+﻿
+       function test1(div) {
+           div.style.transform = "scaleX(2) scaleY(2)";
+           div.style.zIndex = "1";
+       }
+       function test2(div) {
+           div.style.transform = "scaleX(1) scaleY(1)";
+           div.style.zIndex = "0";
+       }
---
+++ js/jjquery.js
@@ -0,0 +1,13 @@
+﻿$(window).on("load", function () {
+    $("h1").fadeIn('slow');
+    $("#slide").fadeIn('slow');
+})
+
+$(window).on("load", function () {
+    $("#i1").fadeIn(400);
+    $("#i2").fadeIn(800);
+    $("#i3").fadeIn(1600);
+    $("#i4").fadeIn(3200);
+    
+    $(".picset").css("display", "inline-block");
+})
